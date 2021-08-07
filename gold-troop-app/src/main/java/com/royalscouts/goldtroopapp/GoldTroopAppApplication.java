1ab9a91b45ac2484f39f68b581e600dee676716f
Juzly Ahamed <juzlyahamed8@gmail.com>
2021-08-07 19:14:46 +0530

    Backend Commit - Juz

---
+++ gold-troop-app/src/main/java/com/royalscouts/goldtroopapp/GoldTroopAppApplication.java
@@ -0,0 +1,13 @@
+package com.royalscouts.goldtroopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldTroopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldTroopAppApplication.class, args);
+	}
+
+}
